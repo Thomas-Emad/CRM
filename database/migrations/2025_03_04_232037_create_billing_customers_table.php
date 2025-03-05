fd7dcc6b216b18261ea4ba9634755fecfe781df1
Thomas-Emad <tomtom22006@gmail.com>
2025-03-05 23:04:08 +0200

    Fix delete Customer

--- database/migrations/2025_03_04_232037_create_billing_customers_table.php
+++ database/migrations/2025_03_04_232037_create_billing_customers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('billing_customers', function (Blueprint $table) {
             $table->id();
             $table->foreignId('country_id')->nullable()->constrained()->references('id')->on('countries')->noActionOnDelete();
-            $table->foreignId('customer_id')->constrained()->references('id')->on('leads')->noActionOnDelete();
+            $table->foreignId('customer_id')->constrained()->references('id')->on('leads')->cascadeOnDelete();
             $table->string('street')->nullable();
             $table->string('city')->nullable();
             $table->string('zip_code')->nullable();
